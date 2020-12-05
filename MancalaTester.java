dd95dfd2edc2b81eecac8e40133cd2962bf277a0
SLiu2022 <73728451+SLiu2022@users.noreply.github.com>
2020-12-04 17:10:01 -0800

    Delete MancalaTester.java

--- MancalaTester.java
+++
@@ -1,8 +0,0 @@
-public class MancalaTester {
-	public static void main(String[] args) {
-		Model model = new Model();
-		View initiate = new View(model);
-		model.attach(initiate);
-		initiate.selectStone();
-	}
-} 
